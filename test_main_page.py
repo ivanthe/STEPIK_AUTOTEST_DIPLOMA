35ce9df4d900005e02555ede15db9279c17fe49d
ivanthe <x6027858x@gmail.com>
2023-05-22 12:19:53 +0300

    First commit. Open testing page

--- test_main_page.py
+++ test_main_page.py
@@ -4,5 +4,5 @@ from selenium.webdriver.common.by import By
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    browser.find_element(By.CSS_SELECTOR, "#login_link").click()
+
