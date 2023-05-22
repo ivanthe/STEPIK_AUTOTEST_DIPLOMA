206c2c115617a4bf8030a2a10f68019738898f95
ivanthe <x6027858x@gmail.com>
2023-05-22 14:29:43 +0300

    added test_main_page code and updated base_page and main_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,7 +2,7 @@
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
-        self.url - url
+        self.url = url
 
     def open(self):
         self.browser.get(self.url)
